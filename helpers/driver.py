040c286ccd27ea10247c84d14007bcd90c5edfae
kauldisha <30968897+kauldisha@users.noreply.github.com>
2020-05-17 20:46:24 +0530

    driver path

--- helpers/driver.py
+++ helpers/driver.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications" : 2}
 chrome_options.add_experimental_option("prefs", prefs)
-driver = webdriver.Chrome(chrome_options=chrome_options , executable_path='C:\Users\Shristi\PycharmProjects\yatra\chromedriver.exe')
+driver = webdriver.Chrome(chrome_options=chrome_options , executable_path='..\..\chromedriver.exe')
 
 #driver.close()
 #driver.quit()
