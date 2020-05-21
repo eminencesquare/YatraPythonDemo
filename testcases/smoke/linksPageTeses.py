fba7ab1fa3440e8ce746de5dfeda438582a2cfc4
kauldisha <30968897+kauldisha@users.noreply.github.com>
2020-05-21 20:08:55 +0530

    link verification

--- testcases/smoke/linksPageTeses.py
+++ testcases/smoke/linksPageTeses.py
@@ -1,14 +1,14 @@
 import unittest
-
 import time
-
 from helpers.driver import driver
 from homePageTests import MyTestCase
+from selenium.webdriver import ActionChains
+
 
 
 class linkVerification(unittest.TestCase):
 
- def testName1(self):
+ def test_name1(self):
      print("finish")
      time.sleep(20)
      driver.find_element_by_css_selector('a[title="Flights"]').click()
@@ -23,8 +23,22 @@ class linkVerification(unittest.TestCase):
      driver.back()
      driver.find_element_by_css_selector("#booking_engine_adventures").click()
      time.sleep(5)
-     more= driver.find_element_by_css_selector(".list-more-tab")
-     time.sleep(20)
+     handles = driver.window_handles
+     size = len(handles)
+     print(size)
+     driver.switch_to.window(handles[1])
+     driver.close()
+     print("22")
+     driver.switch_to.window(handles[0])
+     # driver.find_element_by_css_selector(".i.we_close").click()
+     more= driver.find_element_by_css_selector('.list-more-tab')
+     hover = ActionChains(driver).move_to_element(more)
+     hover.perform()
+     time.sleep(2)
+
+
+def header(self):
+    time.sleep(20)
 if __name__ == '__main__':
-    MyTestCase.testName()
-    linkVerification.testName1()
+    MyTestCase.test_name()
+    linkVerification.test_name1()
--- testcases/smoke/postLogin.py
+++ testcases/smoke/postLogin.py
@@ -1,30 +1,36 @@
 import unittest
-from testcases.smoke import homePageTests
 from helpers.driver import driver
 from homePageTests import MyTestCase
 import time
+from selenium.webdriver import ActionChains
 
 class postLoginVerivication(unittest.TestCase):
+    def sign_in(self):
+        Account = driver.find_element_by_xpath("//a[contains(text(),'My Account')]")
+        hover = ActionChains(driver).move_to_element(Account)
+        hover.perform()
+        driver.find_element_by_css_selector("#signInBtn").click()
+        time.sleep(1)
+        driver.find_element_by_css_selector("#login-input").send_keys("kauldisha24@gmail.com")
+        driver.find_element_by_css_selector("#login-continue-btn").click()
+        time.sleep(2)
+        driver.find_element_by_css_selector(".text-box-full.mt10.wd100").send_keys("Ohmygod24")
+        time.sleep(2)
+        driver.find_element_by_css_selector("#login-submit-btn").click()
+        time.sleep(5)
+        driver.find_element_by_css_selector('a[title="disha"]').is_displayed()
+        time.sleep(2)
 
-    def signIn(self):
-      driver.find_element_by_xpath("//a[contains(text(),'My Account')]").click()
-      driver.find_element_by_css_selector("#signInBtn").click()
-      time.sleep(10)
-      driver.find_element_by_css_selector("#login-input").send_keys("7415009986")
-      driver.find_element_by_css_selector("login-continue-btn").click()
-      driver.find_element_by_css_selector("#login-password").send_keys("Ohmygod24")
-      driver.find_element_by_css_selector("login-continue-btn").click()
-      driver.find_element_by_css_selector('a[title="disha"]').is_displayed()
-    time.sleep(20)
     # def header(self):
-    #
-    #
+    #   time.sleep(20)
     #
     # def footer(self):
-
+    #    time.sleep(20)
 
 if __name__ == '__main__':
     MyTestCase.testName()
-    postLoginVerivication.signIn()
+    plv=postLoginVerivication
+
+    plv.sign_in()
     # postLoginVerivication.header()
     # postLoginVerivication.footer()
